e1d861da93496a4e84d73bdd4eb6cdb93263a253
daniel_lyapin <97834159+dania17@users.noreply.github.com>
2022-01-18 23:51:41 +0100

    Update Todo.js

--- src/Component/Todo.js
+++ src/Component/Todo.js
@@ -2,13 +2,20 @@ import React from "react";
 import daniel from "./daniel.jpg"; 
 
 function Todo(){
+
     return(
         <div className="Todo">
-            <header> <img src={daniel} height={300}  />
-            </header>
-            <header><h5>Daniel Lyapin</h5>
-            </header>
+                <main> <img src={daniel} height={300}  />
+                </main>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
+     <br></br>
         </div>
-    );
+    ) 
 }
 export default Todo;
